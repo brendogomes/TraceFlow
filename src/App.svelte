861a6267886e73aa9b3ec827f00ea3a756d1ae36
“Brendo” <brendo.gomes@educbank.com.br>
2025-04-01 12:28:17 -0300

    feat: width limit

--- src/App.svelte
+++ src/App.svelte
@@ -277,7 +277,7 @@
         copiedStates[id] = false;
       }, 3000);
     } catch (err) {
-      console.error('Failed to copy:', err);
+      console.error("Failed to copy:", err);
     }
   }
 
@@ -529,23 +529,51 @@
                               <div class="mt-2">
                                 <div class="flex justify-between items-center">
                                   <span
-                                    class="font-medium {isDarkMode
+                                    class="text-sm font-medium {isDarkMode
                                       ? 'text-gray-300'
                                       : 'text-gray-700'}">Request Payload:</span
                                   >
                                   <button
                                     class="p-1 rounded hover:bg-gray-200 dark:hover:bg-gray-600"
-                                    on:click|stopPropagation={() => copyToClipboard(JSON.stringify(request.requestBody, null, 2), `${request.id}-request`)}
+                                    on:click|stopPropagation={() =>
+                                      copyToClipboard(
+                                        JSON.stringify(
+                                          request.requestBody,
+                                          null,
+                                          2
+                                        ),
+                                        `${request.id}-request`
+                                      )}
                                     title="Copy to clipboard"
                                   >
                                     {#if copiedStates[`${request.id}-request`]}
-                                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-green-500" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-4 w-4 text-green-500"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                      >
+                                        <path
+                                          fill-rule="evenodd"
+                                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                          clip-rule="evenodd"
+                                        />
                                       </svg>
                                     {:else}
-                                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 {isDarkMode ? 'text-gray-400' : 'text-gray-600'}" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
-                                        <path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z" />
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-4 w-4 {isDarkMode
+                                          ? 'text-gray-400'
+                                          : 'text-gray-600'}"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                      >
+                                        <path
+                                          d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"
+                                        />
+                                        <path
+                                          d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"
+                                        />
                                       </svg>
                                     {/if}
                                   </button>
@@ -553,7 +581,7 @@
                                 <div
                                   class="mt-1 rounded p-2 {isDarkMode
                                     ? 'bg-gray-700'
-                                    : 'bg-gray-50'}"
+                                    : 'bg-gray-50'} max-h-[200px] overflow-y-auto"
                                 >
                                   <pre
                                     class="text-xs overflow-x-auto {isDarkMode
@@ -571,23 +599,51 @@
                               <div class="mt-2">
                                 <div class="flex justify-between items-center">
                                   <span
-                                    class="font-medium {isDarkMode
+                                    class="text-sm font-medium {isDarkMode
                                       ? 'text-gray-300'
                                       : 'text-gray-700'}">Response Body:</span
                                   >
                                   <button
                                     class="p-1 rounded hover:bg-gray-200 dark:hover:bg-gray-600"
-                                    on:click|stopPropagation={() => copyToClipboard(JSON.stringify(request.responseBody, null, 2), `${request.id}-response`)}
+                                    on:click|stopPropagation={() =>
+                                      copyToClipboard(
+                                        JSON.stringify(
+                                          request.responseBody,
+                                          null,
+                                          2
+                                        ),
+                                        `${request.id}-response`
+                                      )}
                                     title="Copy to clipboard"
                                   >
                                     {#if copiedStates[`${request.id}-response`]}
-                                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-green-500" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-4 w-4 text-green-500"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                      >
+                                        <path
+                                          fill-rule="evenodd"
+                                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                          clip-rule="evenodd"
+                                        />
                                       </svg>
                                     {:else}
-                                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 {isDarkMode ? 'text-gray-400' : 'text-gray-600'}" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
-                                        <path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z" />
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-4 w-4 {isDarkMode
+                                          ? 'text-gray-400'
+                                          : 'text-gray-600'}"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                      >
+                                        <path
+                                          d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"
+                                        />
+                                        <path
+                                          d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"
+                                        />
                                       </svg>
                                     {/if}
                                   </button>
@@ -595,7 +651,7 @@
                                 <div
                                   class="mt-1 rounded p-2 {isDarkMode
                                     ? 'bg-gray-700'
-                                    : 'bg-gray-50'}"
+                                    : 'bg-gray-50'} max-h-[200px] overflow-y-auto"
                                 >
                                   <pre
                                     class="text-xs overflow-x-auto {isDarkMode
